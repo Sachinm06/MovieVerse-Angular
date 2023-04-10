741aeb55169fc917d6d0940c062a731f83726d90
Sachin M Sibichan <sachinm06@aol.com>
2023-04-11 00:05:37 +0530

    new changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  trendingMovies: any
+  theaterMovies: any
+  popularMovies:any
+
+  constructor(private http: HttpClient, private router:Router) { }
+
+  ngOnInit(): void {
+    this.getTrendingMovies();
+    this.getTheaterMovies();
+    this.getPopularMovies();
+  }
+  getTrendingMovies() {
+    this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe((movies: any) => {
+      this.trendingMovies = movies;
+      console.log(this.trendingMovies);
+
+    })
+  }
+
+  getTheaterMovies() {
+    this.http.get('http://localhost:4200/assets/data/theatre-movies.json').subscribe((movies: any) => {
+      this.theaterMovies = movies;
+      console.log(this.trendingMovies);
+
+    })
+  }
+
+  getPopularMovies() {
+    this.http.get('http://localhost:4200/assets/data/popular-movies.json').subscribe((movies: any) => {
+      this.popularMovies = movies;
+      console.log(this.trendingMovies);
+
+    })
+  }
+
+  goToMovie(type:string, id:string){
+this.router.navigate(['movie',type,id])
+  }
+
+
 
 }
